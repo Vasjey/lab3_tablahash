a11499227179b790cf5da6bccd091d85e2abc467
Ignacio Daniel Araya Zamorano <rilianx@gmail.com>
2026-02-26 12:47:32 -0300

    Add function stubs for HashMap operations
    Implemented function stubs for HashMap operations including createMap, insertMap, searchMap, eraseMap, firstMap, nextMap, and enlarge.

--- hashmap.c
+++ hashmap.c
@@ -38,34 +38,55 @@ int is_equal(void* key1, void* key2){
     return 0;
 }
 
-
-void insertMap(HashMap * map, char * key, void * value) {
-
-
-}
-
-void enlarge(HashMap * map) {
-    enlarge_called = 1; //no borrar (testing purposes)
-
-
-}
-
+// 1. Implemente la función createMap en el archivo hashmap.c. 
+// Esta función crea una variable de tipo HashMap, inicializa el arreglo de buckets con casillas nulas, inicializa el resto de variables y retorna el mapa. 
+// Inicialice el índice current a -1.
 
 HashMap * createMap(long capacity) {
 
     return NULL;
 }
 
-void eraseMap(HashMap * map,  char * key) {    
+// 2. Implemente la función void insertMap(HashMap * map, char * key, void * value). 
+// Esta función inserta un nuevo dato (key,value) en el mapa y actualiza el índice current a esa posición. 
+// Recuerde que para insertar un par (clave,valor) debe:
+//    a - Aplicar la función hash a la clave para obtener la posición donde debería insertar el nuevo par
+//    b - Si la casilla se encuentra ocupada, avance hasta una casilla disponible (método de resolución de colisiones). 
+//        Una casilla disponible es una casilla nula, pero también una que tenga un par inválido (key==NULL).
+//    c - Ingrese el par en la casilla que encontró.
+// No inserte claves repetidas. Recuerde que el arreglo es circular. Recuerde actualizar la variable size.
 
+void insertMap(HashMap * map, char * key, void * value) {
 
 }
+
+// 3. Implemente la función Pair * searchMap(HashMap * map, char * key), la cual retorna el Pair asociado a la clave ingresada. 
+// Recuerde que para buscar el par debe:
+//   a - Usar la función hash para obtener la posición donde puede encontrarse el par con la clave
+//   b - Si la clave no se encuentra avance hasta encontrarla (método de resolución de colisiones)
+//   c - Si llega a una casilla nula, retorne NULL inmediatamente (no siga avanzando, la clave no está)
+// Recuerde actualizar el índice current a la posición encontrada. Recuerde que el arreglo es circular.
 
 Pair * searchMap(HashMap * map,  char * key) {   
 
 
     return NULL;
 }
+
+// 4. Implemente la función void eraseMap(HashMap * map, char * key). 
+// Está función elimina el dato correspondiente a la clave key. 
+// Para hacerlo debe buscar el dato y luego marcarlo para que no sea válido. 
+// No elimine el par, sólo invalídelo asignando NULL a la clave (pair->key=NULL). 
+// Recuerde actualizar la variable size.
+
+void eraseMap(HashMap * map,  char * key) {    
+
+
+}
+
+// 5. Implemente las funciones para recorrer la estructura: Pair * firstMap(HashMap * map) retorna el primer Pair válido del arreglo buckets. 
+// Pair * nextMap(HashMap * map) retorna el siguiente Pair del arreglo buckets a partir índice current. 
+// Recuerde actualizar el índice.
 
 Pair * firstMap(HashMap * map) {
 
@@ -76,3 +97,26 @@ Pair * nextMap(HashMap * map) {
 
     return NULL;
 }
+
+
+// 6.- Implemente la función void enlarge(HashMap * map). Esta función agranda la capacidad del arreglo buckets y reubica todos sus elementos. 
+// Para hacerlo es recomendable mantener referenciado el arreglo actual/antiguo de la tabla con un puntero auxiliar. 
+// Luego, los valores de la tabla se reinicializan con un nuevo arreglo con el doble de capacidad. 
+// Por último los elementos del arreglo antiguo se insertan en el mapa vacío con el método insertMap. 
+// Puede seguir los siguientes pasos:
+//   a - Cree una variable auxiliar de tipo Pair** para matener el arreglo map->buckets (old_buckets);
+//   b - Duplique el valor de la variable capacity.
+//   c - Asigne a map->buckets un nuevo arreglo con la nueva capacidad.
+//   d - Inicialice size a 0.
+//   e - Inserte los elementos válidos del arreglo old_buckets en el mapa (use la función insertMap que ya implementó).
+
+void enlarge(HashMap * map) {
+    enlarge_called = 1; //no borrar (testing purposes)
+
+
+}
+
+
+
+
+
