0e012f0ed614cf8450a26d699ef9b2679ae274a4
Your Name <you@example.com>
2026-04-16 18:28:32 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -45,8 +45,17 @@ int is_equal(void* key1, void* key2){
 // Inicialice el índice current a -1.
 
 HashMap * createMap(long capacity) {
-
-    return NULL;
+    HashMap *map=(HasMap*)malloc(sizeof(HashMap));
+    if(map==NULL)return NULL;
+    map->buckets=(Pair **)calloc(capacity,sizeof(Pair *));
+    if(map->buckets==NULL){
+        free(map);
+        return NULL;
+        }
+    map->size=0;
+    map->capacity=capacity;
+    map->current=-1;
+    return map;
 }
 
 // 2. Implemente la función void insertMap(HashMap * map, char * key, void * value). 
